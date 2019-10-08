9c2e4b57bd7f6a72111e9a134e2b60635ba603b4
Keillan Cruickshank <cruickshankxkeillan90@gmail.com>
2019-10-08 15:18:04 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,29 @@
 def using_include(array, element)
-
+  cat = ["Luna", "Garfield", "Tom"]
+  cat.include?("Tom")
 end
 
 def using_sort(array)
-
+  anime = ["Rurouni Kenshin", "Gundam Wing", "Demon Slayer", "Inuyasha", "wow"]
+  anime.sort
 end
 
 def using_reverse(array)
-
+  fun = ["yellow", "blue", "tandem", "arrays!"]
+  fun.reverse
 end
 
 def using_first(array)
-
+  words = ["wow", "damn", "boy", "facts"]
+  words.first
 end
 
 def using_last(array)
-
+  l = ["hello", "top", "vers", "chorus", "arrays!"]
+  l.last
 end
 
 def using_size(array)
-
+  a = ["boom", "wail", "block", "house", "rum", "vodka"]
+  a.size
 end
